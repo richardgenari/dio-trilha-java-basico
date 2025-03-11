7828d226bca9de1efab8458dd50c3e5cdc18684b
richardgenari <richardgenari@yahoo.com.br>
2025-03-11 10:09:53 -0300

    atualizando mensagem de saudação e adicionando variáveis de exemplo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("ola mundo");
+                int idade= 10;
+                System.out.println(idade);
+                double salarioMinimo = 2500;
+                String nome = "richard";
+                System.out.println("nome: "+ nome + " - " + " idade: "+ idade);
+                
     }
 }
