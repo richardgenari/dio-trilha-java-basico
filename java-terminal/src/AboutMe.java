aba8599f97943a8bbbea28c5160cd3737d0b7b5f
richardgenari <richardgenari@yahoo.com.br>
2025-03-17 10:31:17 -0300

    Programa entrada usuario.

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,29 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class AboutMe {
+    public static void main(String[] args)  {
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+       System.out.println("Digite seu nome");
+       String nome = scanner.next();
+
+       System.out.println("Digite seu sobrenome");
+       String sobrenome = scanner.next();
+
+       System.out.println("Digite sua idade");
+       int idade = scanner.nextInt();
+
+       System.out.println("Digite sua altura");
+       double altura = scanner.nextDouble();
+       
+
+       //imprime os dados obtidos pelo usuario
+         System.out.println("Nome: " + nome  + " " +  sobrenome);
+         System.out.println("Idade: " + idade);
+         System.out.println("Altura:" + altura + "Cm");
+
+    }
+}
